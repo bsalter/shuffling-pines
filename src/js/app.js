b1798de4940a4c9e8ad1c49980b3c377a7c69e1d
Benn Salter <bennsalter@gmail.com>
2015-10-13 21:28:08 -0400

    form handling

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,33 @@
 var app = angular.module('shuffling', []);
 
-app.controller('FormController', ['$scope',function($scope){
+app.controller('FormController', ['$scope','Storage',function($scope, storageService) {
+    $scope.submit = function() {
+        var person = {
+            "name":$scope.name,
+            "date":$scope.transition_date,
+            "transportation":$scope.transportation,
+            "location":$scope.location
+        };
+        $scope.name = "";
+        $scope.transition_date = "";
+        $scope.transportation = "pickup";
+        $scope.location = "";
+        storageService.addPerson(person);
+    };
+}]);
+
+app.controller('TabController', [function() {
 
 }]);
 
-app.controller('TabController', [function(){
-
-}]);
+app.factory('Storage', function() {
+    var people = JSON.parse(sessionStorage.getItem('people')) || [];
+    console.log(people);
+    function addPerson(person) {
+        people.push(person);
+        console.log(people);
+    }
+    return {
+        addPerson:addPerson
+    };
+});
