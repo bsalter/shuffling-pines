0c49e443810b5591287611c2987d0c8665cb30ed
Benn Salter <bennsalter@gmail.com>
2015-10-15 18:57:39 -0400

    removed scope

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,57 @@
-var app = angular.module('shuffling', []);
-
-app.controller('FormController', ['$scope','Storage',function($scope, storageService) {
-    $scope.submit = function() {
-        var person = {
-            "name":$scope.name,
-            "date":$scope.transition_date,
-            "transportation":$scope.transportation,
-            "location":$scope.location
+(function() {
+    var app = angular.module('shuffling', []);
+    app.controller('FormController', ['Storage', function (storageService) {
+        var vm = this;
+        vm.transportation = 'pickup';
+        vm.submit = function () {
+            var patient_data = {
+                "name": vm.name,
+                "date": vm.transition_date,
+                "transportation": vm.transportation,
+                "location": vm.location
+            };
+            vm.name = "";
+            vm.transition_date = "";
+            vm.transportation = "pickup";
+            vm.location = "";
+            storageService.addPatient(patient_data);
         };
-        $scope.name = "";
-        $scope.transition_date = "";
-        $scope.transportation = "pickup";
-        $scope.location = "";
-        storageService.addPerson(person);
-        $scope.$emit('tabChanged', 2);
-    };
-}]);
+        vm.checkName = function(name) {
+            return vm.name === name;
+        };
+        vm.checkDate = function(date) {
+            return vm.transition_date === date;
+        };
+        vm.checkTransportation = function(transportation) {
+            return vm.transportation === transportation;
+        };
+        vm.checkLocation = function(location) {
+            return vm.location === location;
+        }
+    }]);
 
-app.controller('TabController', ['$scope', function($scope) {
-    $scope.$on('tabChanged', function(event, tab) {
-        $scope.tab = tab
+    app.controller('TabController', [function () {
+        var vm = this;
+        vm.tab = 1;
+        vm.setTab = function(tabnum) {
+            vm.tab = tabnum;
+        };
+        vm.checkTab = function(tabnum) {
+            return vm.tab === tabnum;
+        };
+
+    }]);
+
+    app.factory('Storage', function () {
+        var patients = JSON.parse(sessionStorage.getItem('patients')) || [];
+        console.log(patients);
+        function addPatient(patient) {
+            patients.push(patient);
+            console.log(patients);
+        }
+
+        return {
+            addPatient: addPatient
+        };
     });
-}]);
-
-app.factory('Storage', function() {
-    var people = JSON.parse(sessionStorage.getItem('people')) || [];
-    console.log(people);
-    function addPerson(person) {
-        people.push(person);
-        console.log(people);
-    }
-    return {
-        addPerson:addPerson
-    };
-});
+})();
