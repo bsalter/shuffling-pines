689f389f39d8f6fe49705361caad4350fbaa2d2d
Benn Salter <bennsalter@gmail.com>
2015-10-18 20:48:36 -0400

    guest list

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@
                 "name": vm.name,
                 "date": vm.transition_date,
                 "transportation": vm.transportation,
-                "location": vm.location
+                "location": vm.location || ""
             };
             vm.name = "";
             vm.transition_date = "";
@@ -42,19 +42,59 @@
         }
     }
 
-    function Storage() {
-        var patients = JSON.parse(sessionStorage.getItem('patients')) || [];
-        console.log(patients);
-        function addPatient(patient) {
-            patients.push(patient);
-            console.log(patients);
-        }
-
-        return {
-            addPatient: addPatient
+    function PatientListController(storageService) {
+        var vm = this;
+        vm.patients = storageService.getPatients();
+        vm.getPatients = function() {
+          return vm.patients;
         };
+        vm.checkLocation = function(location) {
+            return (typeof location !== "");
+        };
+
+    }
+
+    function Storage() {
+        this.patients = JSON.parse(localStorage.getItem('patients')) || [];
+        console.log(this.patients);
+        this.addPatient = function(patient) {
+            this.patients.push(patient);
+            localStorage.setItem('patients',JSON.stringify(this.patients));
+            // make sure we stored it correctly
+            this.patients = JSON.parse(localStorage.getItem('patients'));
+            console.log(this.patients);
+        };
+        this.getPatients = function() {
+            return JSON.parse(localStorage.getItem('patients'));
+        };
+        if(this.patients.length == 0) { // initialize with example data
+            console.log("Begin initialization");
+            var data = {
+                "name":"Frank",
+                "date":"02/10/2015",
+                "transportation":"dropoff",
+                "location":""
+            };
+            this.addPatient(data);
+            data = {
+                "name":"Samantha",
+                "date":"10/15/2015",
+                "transportation":"pickup",
+                "location":"100 Main St., Cambridge, MA 02140"
+            };
+            this.addPatient(data);
+            data = {
+                "name":"Howard",
+                "date":"09/01/2014",
+                "transportation":"dropoff",
+                "location":""
+            };
+            this.addPatient(data);
+        }
+        return this;
     }
     app.controller('FormController', ['Storage', FormController]);
     app.controller('TabController', [TabController]);
+    app.controller('PatientListController', ['Storage', PatientListController]);
     app.factory('Storage', Storage);
 })();
